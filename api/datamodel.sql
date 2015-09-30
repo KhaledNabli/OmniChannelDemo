dfc9ce32f4a036b0273e29a8d4385354a96f3083
khanab85 <khaled@nabli.de>
2015-09-30 16:10:38 +0200

    added database schema

---
+++ api/datamodel.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 30. Sep 2015 um 16:02
+-- Server Version: 5.6.20-log
+-- PHP-Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `omnichanneldemo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `contact_response_history`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_response_history` (
+  `token` varchar(64) NOT NULL,
+  `customer` varchar(64) NOT NULL,
+  `offer` varchar(64) NOT NULL,
+  `channel` varchar(64) NOT NULL,
+  `entrytype` varchar(64) NOT NULL,
+  `responsetype` varchar(64) NOT NULL,
+  `responsedetails` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `customer_offers`
+--
+
+CREATE TABLE IF NOT EXISTS `customer_offers` (
+  `token` varchar(64) NOT NULL,
+  `customer` varchar(64) NOT NULL,
+  `offer` varchar(64) NOT NULL,
+  `score` int(11) NOT NULL,
+  `display_limit` int(11) NOT NULL,
+  `responded` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `demo_config`
+--
+
+CREATE TABLE IF NOT EXISTS `demo_config` (
+`id` int(11) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `config_json` text NOT NULL,
+  `create_dttm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modify_dttm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `demo_config`
+--
+ALTER TABLE `demo_config`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `token` (`token`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `demo_config`
+--
+ALTER TABLE `demo_config`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- api/index.php
+++ api/index.php
@@ -1,12 +1,75 @@
 <?php
+session_set_cookie_params(3600*24*30);
+session_start();
+
+
+
+@$http_proxy = "srv01gr.unx.sas.com:80";
+@$demoScenarioFile = "config.json";
+@$demoConfig = array();
+@$enable_logging = true;
+@$logging_db = array(
+	"host" => "localhost:3306", 
+	"user" => "visionarydemo",
+	"pass" => "Lnj9QqhV89MbtjLW");
+
+
+// mysql connection
+
+
+
+// parse parameters
+if($_SERVER['REQUEST_METHOD'] == "GET") {
+	@$action = $_GET['action'];
+	@$token = $_GET['token'];
+	@$param = $_GET['param'];
+} else {
+	@$action = $_POST['action'];
+	@$token = $_POST['token'];
+	@$param = $_POST['param'];	
+}
 
 
 
 
 
+
+process($action, $param);
+return;
+
+
+function process($token, $action, $param) {
+	if($action == 'reset') {
+		session_destroy();
+		header('Location:'.$_SERVER['PHP_SELF'] );
+		return true;
+	}
+
+	else if($action == 'saveConfig') {
+		@header('Content-type: application/json');
+		@logUsage("DEMO_SAVE", $param, "", "");
+		return true;
+	}
+	else {
+		@header('Content-type: application/json');
+		@logUsage("DEMO_LOADING", $param, "", "");
+		return true;	
+	}
+}
+
+
+
+
+
+
+
+
+/**
+*
+*/
 function getConfig($token) {
 
-	return;
+	return $config;
 }
 
 
@@ -21,15 +84,75 @@ function saveConfig($token, $config) {
 
 
 
+/**
+*
+*/
+function getOffers($token, $customer, $list_size){
+	// calculate score
+
+	// limit number of offers
+
+	// track display information
+}
+
+
+/**
+*
+*/
+function respondToOffer($token, $customer, $offerCd, $responseCd, $channelCd, $details) {
+	// check if offer is valid
+
+	// mark response in data base
+
+	// increase / decrease scores
+
+	// 
+}
+
+
+/**
+*
+*/
+function getCustomerHistory($token, $customer) {
+
+	return $historyList;
+}
 
 
 
+/**
+*
+*/
+function resetDemo() {
+	return $token;
+}
 
 
+function logUsage($eventType, $demoScenario, $detail1, $detail2) {
+	global $enable_logging;
+	global $logging_db;
+
+	if($enable_logging == false) {
+		return false; 
+	}
 
 
+	$userIp = (isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? "Proxy: " . ($_SERVER['HTTP_X_FORWARDED_FOR']) : $_SERVER['REMOTE_ADDR']);
+	$userHost =  gethostbyaddr($_SERVER['REMOTE_ADDR']);
+	$userSystem =  "Computer: " .  ($userHost != null ?  $userHost : "Unknown" ) . ". Browser: " . htmlspecialchars($_SERVER["HTTP_USER_AGENT"]) ;
 
 
+	$link = mysqli_connect($logging_db['host'], $logging_db['user'], $logging_db['pass']);
+	if (!$link) {
+	   	return false;
+	}
+
+	$sqlInsertQuery = "INSERT INTO beacondemo.demo_events (id, session,event_dttm, event_type, user_ip, user_system, user_scenario, detail1, detail2) VALUES (NULL, \"". session_id() ."\" ,CURRENT_TIMESTAMP, \"" . $eventType . "\", \"".$userIp."\",\"".$userSystem."\", \"". addslashes(json_encode($demoScenario)) ."\", \"".$detail1."\",  \"".$detail2."\");";
+	//echo $sqlInsertQuery;
+	mysqli_query($link,$sqlInsertQuery);
+	mysqli_close($link);
+	return true;
+}
 
 
 ?>
